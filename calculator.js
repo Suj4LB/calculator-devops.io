5fda8a6e5ee410cead102198f2e86630e0aee8d0
samarr <143421234+samariium@users.noreply.github.com>
2023-09-08 23:45:12 +0530

    Update calculator.js
    till signs

--- calculator.js
+++ calculator.js
@@ -1,1 +1,22 @@
-Java Script
+//Java Script
+var screen=document.querySelector('#screen');
+  var btn=document.querySelectorAll('.btn');
+  
+   /*============ For getting the value of btn, Here we use for loop ============*/
+    for(item of btn)
+      {
+        item.addEventListener('click',(e)=>{
+          btntext=e.target.innerText;
+
+          if(btntext == 'x')
+          {
+                btntext='*';
+          }
+
+          if(btntext=='÷')
+          {
+                btntext='/';
+          }
+          screen.value+=btntext;
+          });
+      }
