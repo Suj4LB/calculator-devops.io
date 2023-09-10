4c392e69b4d5ad08e3f0b0c9228bf244931d55ef
samarr <143421234+samariium@users.noreply.github.com>
2023-09-10 11:01:08 +0530

    Update app.js
    complete the code for javascript

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var screen=document.querySelector('#screen');
           screen.value+=btntext;
           });
       }
-// code for some functions like  sin,cos,tan etc
+// code for some functions like  sin, cos,tan, etc
 function sin()
     {
         screen.value=math.sin(screen.value);
@@ -46,3 +46,31 @@ function log()
     {
         screen.value=Math.log(screen.value);
     }
+function pi()
+    {
+        screen.value= 3.14159265359;
+    }
+function e()
+    {
+        screen.value=2.71828182846;
+    }
+ function fact()
+    {
+        var i, num, f;
+        f=1
+        num=screen.value;
+        for(i=1; i<=num; i++)
+        {
+            f=f*i;
+        }
+
+        i=i-1;
+
+        screen.value=f;
+    }
+
+    function backspc()
+    {
+        screen.value=screen.value.substr(0,screen.value.length-1);
+    }
+
